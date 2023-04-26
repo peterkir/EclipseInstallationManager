46f929051b3c9318aa569ff9b89d7368b6eb70fa
Alexander Schindler <alex@klib.io>
2023-04-26 12:10:23 +0200

    Icnlud Testannotations to actually test something, cleanUp after test

--- eim.pop/src/eim/pop/EIMTest.java
+++ eim.pop/src/eim/pop/EIMTest.java
@@ -1,5 +1,7 @@
 package eim.pop;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.knowhowlab.osgi.testing.assertions.BundleAssert.assertBundleState;
 import static org.knowhowlab.osgi.testing.assertions.OSGiAssert.getBundleContext;
 import static org.knowhowlab.osgi.testing.assertions.ServiceAssert.assertServiceAvailable;
@@ -13,20 +15,24 @@ import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.knowhowlab.osgi.testing.utils.ServiceUtils;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
 
 import eim.api.EclipseService;
-import junit.framework.TestCase;
 
-public class EIMTest extends TestCase {
+public class EIMTest {
 	
-	private final String SERVICE_BUNDLE = "eim";
+	private final String SERVICE_BUNDLE = "eim.impl";
 	private final BundleContext context = FrameworkUtil.getBundle(EIMTest.class).getBundleContext();
-	private String command;
-	public void setUp() {
+	private static String command;
+	
+	@BeforeAll
+	public static void setUp() {
 		String os = System.getProperty("os.name").toLowerCase();
 		if(os.contains("windows")) {
 			command="C:/Windows/System32/notepad.exe";
@@ -35,6 +41,8 @@ public class EIMTest extends TestCase {
 		}
 	}
 	
+	
+	@Test
 	public void testLifeCycle() throws Exception {
 		assertBundleState(Bundle.ACTIVE, SERVICE_BUNDLE);
 		assertServiceAvailable(EclipseService.class);
@@ -45,6 +53,7 @@ public class EIMTest extends TestCase {
 		assertNotNull(eclService);
 	}
 	
+	@Test
 	public void testStartProcess() {
 		EclipseService eclService = ServiceUtils.getService(context, EclipseService.class);
 		Process p = eclService.startProcess(command, System.getProperty("user.home"), null);
@@ -62,7 +71,18 @@ public class EIMTest extends TestCase {
 		
 	}
 	
-	private Long getPidViaRuntimeMXBean() {
+	@AfterAll
+	public static void cleanUp() {
+		long parentPID = getPidViaRuntimeMXBean();
+		Optional<ProcessHandle> processHandle = ProcessHandle.of(parentPID);
+		processHandle.ifPresent(process -> {
+			process.children().forEach(child -> {
+				child.destroy();
+			});
+		});
+	}
+	
+	private static Long getPidViaRuntimeMXBean() {
 		RuntimeMXBean rtb = ManagementFactory.getRuntimeMXBean();
 		String processName = rtb.getName();
 		Long result = null;
