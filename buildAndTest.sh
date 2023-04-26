db678d9dbf07f840fe582fe57f59a2e046ebc0a8
Alexander Schindler <alex@klib.io>
2023-04-26 12:40:09 +0200

    set-Java -> set-java (case sensitive)

--- buildAndTest.sh
+++ buildAndTest.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(dirname "$SCRIPT")
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/klibio/bootstrap/main/install-klibio.sh)" bash -j
 source ~/.klibio/klibio.sh
-set-Java.sh 17
+set-java.sh 17
 
 ./gradlew \
     --info \
