ae665401a7334853a2b407e729b2eabb9ebfc132
Alexander Schindler <alex@klib.io>
2023-05-07 13:12:58 +0200

    Feature/artifact release (#3)
    * Add basic testing capabilities and a github workflow
    
    * Rename build stage
    
    * Make gradle wrapper executable within workflow
    
    * Move api project to java 17
    
    * Delete eim.pop/rt directory
    
    * Add startProcess testcase
    
    * Remove useless jacoco artifacts from repository
    
    * remove _rt folder form github repo
    
    * ignore *.exec files, remove existing
    
    * Streamline java versions
    
    * Icnlud Testannotations to actually test something, cleanUp after test
    
    * Remove deprecated cast to long
    
    * Add Java 17 to workflow
    
    * source .klibio.sh
    
    * Move setup tasks to script, fix source
    
    * Explicit source command
    
    * set-Java -> set-java (case sensitive)
    
    * Point gradle to klibio installed jdk
    
    * Reintroduce source, add debug
    
    * Source java script
    
    * Only add environmental var when under windows
    
    * Add version file, add release to workflow
    
    * Change workflow artifact uplaod action
    
    * Fix typo in action name
    
    * Fix another typo
    
    * Add brackets for variable evaluation
    
    * Use correct workspace var
    
    * Add release script
    
    * Fix if condition
    
    * Remove == for =
    
    * Add space in fornt of ]]

---
+++ createReleaseAndArtifacts.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# activate bash checks for unset vars, pipe fails
+set -eauo pipefail
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+
+
+version=$(cat version)
+tag=v${version}
+branch=$(echo ${GITHUB_REF#refs/heads/})
+
+if [[ ${branch} = "main" ]]; then
+    gh release create ${tag}\
+        --latest \
+        --title "Eclipse Installation Manager (EIM) v${version}" \
+        --generate-notes \
+        ${{ github.workspace }}/eim/generated/distributions/executable/eim.jar \
+        ${{ github.workspace }}/eim.api/generated/distributions/executable/eim.api.jar
+else
+    echo "# Skipping release because the branch is not main"
+fi
