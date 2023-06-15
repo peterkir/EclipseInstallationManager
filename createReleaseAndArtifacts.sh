ef7ce24ca60fc4d002c2e7c2bb6d7a04d2a8bd8a
Alexander Schindler <alex@klib.io>
2023-06-15 17:13:17 +0200

    Remove uppercase vars, change workingdir in artifact script to workspace

--- createReleaseAndArtifacts.sh
+++ createReleaseAndArtifacts.sh
@@ -4,7 +4,8 @@ set -eauo pipefail
 script=$(readlink -f "$0")
 script_dir=$(dirname "$script")
 
-cd ${script_dir}
+cd ${github_workspace}
+pwd
 
 version=$(cat version)
 apiVersion=$(cat apiVersion)
@@ -12,16 +13,16 @@ tag=v${version}
 branch=$(echo ${GITHUB_REF#refs/heads/})
 
 # rename jars to include the version
-mv ${GITHUB_WORKSPACE}/eim/generated/distributions/executable/eim.jar ${GITHUB_WORKSPACE}/eim/generated/distributions/executable/eim_${version}.jar
-mv ${GITHUB_WORKSPACE}/eim.api/generated/eim.api.jar ${GITHUB_WORKSPACE}/eim.api/generated/eim.api_${apiVersion}.jar
+mv ${github_workspace}/eim/generated/distributions/executable/eim.jar ${github_workspace}/eim/generated/distributions/executable/eim_${version}.jar
+mv ${github_workspace}/eim.api/generated/eim.api.jar ${github_workspace}/eim.api/generated/eim.api_${apiVersion}.jar
 
 if [[ ${branch} = "main" ]]; then
     gh release create ${tag}\
         --latest \
         --title "Eclipse Installation Manager (EIM) v${version}" \
         --generate-notes \
-        ${GITHUB_WORKSPACE}/eim/generated/distributions/executable/eim_${version}.jar \
-        ${GITHUB_WORKSPACE}/eim.api/generated/eim.api_${apiVersion}.jar
+        ${github_workspace}/eim/generated/distributions/executable/eim_${version}.jar \
+        ${github_workspace}/eim.api/generated/eim.api_${apiVersion}.jar
 else
     echo "# Skipping release because the branch is not main"
 fi
